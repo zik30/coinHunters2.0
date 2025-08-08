06a4fc5e373760fdbbd88f7c404ca8568dc5e661
zik30 <ainazik.ishenbek@gmail.com>
2025-08-08 22:37:20 +0600

    f

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import commonjs from "@rollup/plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   resolve: {
     alias: {
       "@pages": "/src/pages",
